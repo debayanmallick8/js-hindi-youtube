0cd7f869bc3ea8b98a196709e49dadb198adc830
debayanmallick8 <debayanmallick69@gmail.com>
2025-02-16 07:31:31 +0000

    detatypes

---
+++ 01_bacics/01_variables.js
@@ -0,0 +1,17 @@
+const accountId = 144553
+let accountemail = "debayan@google.com"
+var password = "12345"
+accountCity = "bongaon"
+let accountstate;
+
+// accountId = 2 // not allowed
+
+accountemail = "ab@ab.com"
+accountpassword = "12345"
+accountCity = "bengaluru"
+
+console.log(accountId);
+
+
+
+console.table([ accountId,accountemail,accountpassword,accountCity ,accountstate])
